8ecfd35a03a7c291173725a0c4fd57361d6dbe28
Your Name <abirdutta90990@gmail.com>
2023-09-30 20:28:09 +0530

    UI changes done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,53 +3,26 @@ import { BiDownArrowAlt } from "react-icons/bi";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import { getAddress } from "../../../Store/Getters";
-import {useRecoilValue, useSetRecoilState} from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { wallet } from "../../../Store/Variables";
 import BridgeKaiABI from "../abis/BridgeKai.json";
 import BridgeKaiBPABI from "../abis/BridgeKaiBP.json";
-
-import { burnFromKAIBP, MintOnKAI, MintOnKAIBP,  burnFromKAI } from "./web3";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { burnFromKAIBP, MintOnKAI, MintOnKAIBP, burnFromKAI } from "./web3";
 const home = () => {
-  const walletAddress=useRecoilValue(getAddress);
-  const setWalletAddress=useSetRecoilState(wallet);
-  const [fromToken, setFromToken]=useState("");
-  const[toToken, setToToken]=useState("");
-  useEffect(()=>{
-    getCurrent();
-    newWallet();
+  const walletAddress = useRecoilValue(getAddress);
+  const setWalletAddress = useSetRecoilState(wallet);
+  const [fromToken, setFromToken] = useState("");
+  const [toToken, setToToken] = useState("");
+  const [fromAmount, setFromAmount] = useState("");
+  const [toAmount, setToAmount] = useState("");
+  const notifySuccess = (successText) => toast.success(successText);
+  const notifyError = (errorText) => toast.error(errorText);
+  useEffect(() => {
+    getCurrent(setWalletAddress);
+    newWallet(setWalletAddress);
   })
-
-
-
-  async function getCurrent(){
-    if(typeof window !='undefined' && typeof window.ethereum !='undefined'){
-     try{
-       const accounts=await window.ethereum.request({method: "eth_requestAccounts"});
-       if(accounts.length>0){
-         setWalletAddress({
-          address: accounts[0]
-         });
-         console.log(accounts[0]);
-       }else{
-         console.log("Connect with metamask");
-       }
-    }catch(e){
-     console.log(e);
-    }
- }
-}
-
-async function newWallet(){
- if(typeof window !='undefined' && typeof window.ethereum !='undefined'){
-   window.ethereum.on("accountsChanged", (accounts)=>{
-     setWalletAddress({address: accounts[0]});
-   })
- }else{
-   setWalletAddress("");
-   console.log("please install metamask");
- }
-}
-
 
 
 
@@ -74,11 +47,11 @@ async function newWallet(){
           // Create a contract instance
           const BridgeKaicontractAddress = "0x9135D33304cdC48AAEDe2daD2CCf51D91546CB1F";
           const BridgeKaiBPcontractAddress = "0x9135D33304cdC48AAEDe2daD2CCf51D91546CB1F";
-          const BridgeKaicontract = new ethers.Contract(BridgeKaicontractAddress,BridgeKaiABI, signer);
-          const BridgeKaiBPcontract = new ethers.Contract(BridgeKaiBPcontractAddress,BridgeKaiBPABI, signer);
+          const BridgeKaicontract = new ethers.Contract(BridgeKaicontractAddress, BridgeKaiABI, signer);
+          const BridgeKaiBPcontract = new ethers.Contract(BridgeKaiBPcontractAddress, BridgeKaiBPABI, signer);
 
           let accounts = await provider.send("eth_requestAccounts", []);
-          let userAddress= accounts[0];
+          let userAddress = accounts[0];
 
           // Update the web3 state
           setWeb3State({
@@ -98,7 +71,7 @@ async function newWallet(){
     };
 
     initializeWeb3();
-  },[]);
+  }, []);
 
   // return web3State;
 
@@ -109,23 +82,31 @@ async function newWallet(){
   async function handleSwap() {
     console.log(web3State)
     if (fromToken == toToken) {
-      return alert("You can't swap between two tokens");
-    } 
+      notifyError("You can't swap between two tokens");
+      return
+    }
+    if (!fromAmount || !toAmount) {
+      return notifyError("Please select some amount");
+    }
 
-    if(fromToken=="KAI-BEP20"){
+    if (fromToken == "KAI-BEP20") {
       try {
         const response = await burnFromKAI(web3State, walletAddress, "100000000000");
         console.log(response);
         const mint = await MintOnKAIBP(web3State, walletAddress, "100000000000");
+        return notifySuccess("Transaction successful");
       } catch (e) {
         console.log(e);
+        return notifyError("Soe error occured");
       }
-    }else{
-      try{
-        const response=await burnFromKAIBP(web3State, walletAddress, "100000000000");
-        const mint=await MintOnKAI(web3State, walletAddress, "100000000000");
-      }catch(e){
+    } else {
+      try {
+        const response = await burnFromKAIBP(web3State, walletAddress, "100000000000");
+        const mint = await MintOnKAI(web3State, walletAddress, "100000000000");
+        return notifySuccess("Transaction Successful")
+      } catch (e) {
         console.log(e);
+        return notifyError("Some error occured")
       }
     }
   }
@@ -139,15 +120,17 @@ async function newWallet(){
         <div className="flex justify-between items-center px-6">
           <div className="flex gap-4 items-center">
             <h1 className="text-white text-[20px] font-[400]">Swap</h1>
-           
+
           </div>
           <AiOutlineSetting className="w-6 h-6 text-white" />
         </div>
         <div className="relative">
           <div className="flex items-center justify-between bg-[#1B1B1B] text-start px-4 mx-2 py-6 lg:py-3 mt-5 rounded-xl hover:border hover:border-gray-800">
             <div className="text-gray-500">
-              <h1 className="text-[16px] font-[700] leading-[20px]">You pay</h1>
-              <p className="text-[28px] font-[600]">0</p>
+              <h1 className="text-[16px] font-[700] leading-[20px]" >You pay</h1>
+              <input type="text" placeholder="0" className="text-[28px] font-[600] w-[50%] bg-[#1B1B1B] outline-none" onChange={(e) => {
+                setFromAmount(e.target.value);
+              }}></input>
               {/* <p className="text-[16px] font-normal text-gray-400 lg:block hidden">
                 $1,660.61
               </p> */}
@@ -156,14 +139,14 @@ async function newWallet(){
               <select
                 className="px-1 ml-10 bg-[#131313] text-white border border-gray-800 py-1 text-[20px] font-[600] rounded-2xl"
                 id="schedule"
-                onChange={(e)=>{
+                onChange={(e) => {
                   setFromToken(e.target.value);
                 }}
               >
-                <option >KAI</option>
+                <option >Select...</option>
                 <option >KAI-BEP20</option>
                 <option >Mainnet KAI</option>
-                
+
               </select>
             </div>
           </div>
@@ -173,7 +156,9 @@ async function newWallet(){
           <div className="flex items-center justify-between text-start bg-[#1B1B1B] px-4 mx-2 py-6 lg:py-3 mt-1 rounded-xl hover:border hover:border-gray-800">
             <div className="text-gray-500">
               <h1 className="text-[16px] font-[700] leading-[20px]">You pay</h1>
-              <p className="text-[28px] font-[600]">0</p>
+              <input placeholder="0" type="number" className="text-[28px] font-[600] w-[50%] bg-[#1B1B1B] outline-none" onChange={(e) => {
+                setToAmount(e.target.value)
+              }} ></input>
               {/* <p className="text-[16px] font-normal text-gray-400 lg:block hidden">
                 $1,660.61
               </p> */}
@@ -182,7 +167,7 @@ async function newWallet(){
               <select
                 className="px-2 bg-[#F476FA] text-white border border-white py-1 font-[600] text-[20px] rounded-3xl"
                 id="schedule"
-                onChange={(e)=>{
+                onChange={(e) => {
                   setToToken(e.target.value);
                 }}
               >
@@ -193,8 +178,20 @@ async function newWallet(){
             </div>
           </div>
           <button className="bg-[#311C31] mt-2 text-[#F275F8] mx-2 px-5 w-[97%] py-2 rounded-2xl font-[700] text-[20px]" onClick={handleSwap}>
-          {walletAddress.length>0 ?("Swap"): ("Connect Wallet")}
+            {walletAddress.length > 0 ? ("Swap") : ("Connect Wallet")}
           </button>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
         </div>
       </div>
     </div>
@@ -202,3 +199,34 @@ async function newWallet(){
 };
 
 export default home;
+
+
+async function getCurrent(setWalletAddress) {
+  if (typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+    try {
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+      if (accounts.length > 0) {
+        setWalletAddress({
+          address: accounts[0]
+        });
+        console.log(accounts[0]);
+      } else {
+        console.log("Connect with metamask");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+
+async function newWallet(setWalletAddress) {
+  if (typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+    window.ethereum.on("accountsChanged", (accounts) => {
+      setWalletAddress({ address: accounts[0] });
+    })
+  } else {
+    setWalletAddress("");
+    console.log("please install metamask");
+  }
+}
